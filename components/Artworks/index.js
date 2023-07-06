b6d24c51f4584ffeb54c6cf9463974836ab0b2a5
Mona Herter <hertermona@gmail.com>
2023-07-06 15:52:51 +0200

    add fetch function

---
+++ components/Artworks/index.js
@@ -0,0 +1,28 @@
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+export default function Artworks() {
+  const { data, error, isLoading } = useSWR(
+    "https://api.smk.dk/api/v1/art/?object_number=kms1&lang=en",
+    fetcher
+  );
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+}
