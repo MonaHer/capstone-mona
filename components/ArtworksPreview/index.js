1bf0da2d70197c607dd62aeefc9009f143ce2f49
Mona Herter <hertermona@gmail.com>
2023-07-07 12:33:30 +0200

    restructure components

--- components/ArtworksPreview/index.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function ArtworksPreview({ image, title }) {
-  return (
-    <>
-      <Image src={image} alt={title} height={300} width={300} />
-    </>
-  );
-}
