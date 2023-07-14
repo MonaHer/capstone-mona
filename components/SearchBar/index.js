435c80b38863d8370aa21fa364800a99e18fa140
Mona Herter <hertermona@gmail.com>
2023-07-14 11:36:25 +0200

    create search function

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,25 +1,87 @@
 import { useState } from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function SearchBar({ artworks }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchPerformed, setSearchPerformed] = useState(false);
 
   function handleInputChange(e) {
-    setSearchTerm(e.target.value);
+    const { value } = e.target;
+    setSearchTerm(value);
+    if (value === "") {
+      setSearchResults([]);
+    } else {
+      handleSearch(value);
+    }
   }
 
-  function handleSearch(searchTerm) {
-    const filteredTitles = artworks.filter((artwork) =>
-      artwork.titles[0].title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(filteredTitles);
+  function handleSearch(value) {
+    const filteredArtworks = artworks.items.filter((artwork) => {
+      const titleMatch = artwork.titles[0].title
+        .toLowerCase()
+        .includes(value.toLowerCase());
+      const creatorMatch =
+        artwork.production[0].creator_forename
+          .toLowerCase()
+          .includes(value.toLowerCase()) ||
+        artwork.production[0].creator_surname
+          .toLowerCase()
+          .includes(value.toLowerCase()) ||
+        `${artwork.production[0].creator_forename} ${artwork.production[0].creator_surname}`
+          .toLowerCase()
+          .includes(value.toLowerCase());
+      return titleMatch || creatorMatch;
+    });
+    setSearchResults(filteredArtworks);
+    setSearchPerformed(true);
   }
 
   return (
     <>
       <input type="text" value={searchTerm} onChange={handleInputChange} />
-      <button type="submit" onHandleSearch={handleSearch}>
-        Search
-      </button>
+
+      <StyledList>
+        {searchResults.map(({ id, image_thumbnail, titleText }) => {
+          return (
+            <StyledListItem key={id}>
+              <Link href={`/artwork-info/${id}`}>
+                <StyledImage
+                  src={image_thumbnail}
+                  alt={titleText}
+                  width={200}
+                  height={200}
+                />
+              </Link>
+            </StyledListItem>
+          );
+        })}
+      </StyledList>
     </>
   );
 }
+
+const StyledList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
+const StyledImage = styled(Image)`
+  height: 90%;
+  width: 90%;
+`;
+
+const StyledListItem = styled.li`
+  text-align: center;
+`;
+
+// {
+//   /* <button type="submit" onClick={handleSearch}>
+// Search
+// </button>
+// {searchPerformed && searchResults.length === 0 && (
+// <p>No results found.</p>
+// )} */
+// }
---
+++ pages/search/index.js
@@ -0,0 +1,9 @@
+import SearchBar from "@/components/SearchBar";
+
+export default function SearchPage({ artworks }) {
+  return (
+    <>
+      <SearchBar artworks={artworks} />
+    </>
+  );
+}
