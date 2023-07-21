bffeee731d33c13250a87d110c3fba24e42a7364
Mona Herter <hertermona@gmail.com>
2023-07-21 11:04:29 +0200

    create randomcover without time intervall

--- components/RandomCover/index.js
+++ components/RandomCover/index.js
@@ -1,14 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
+import { styled } from "styled-components";
 
 export default function RandomCover({ artworks }) {
+  const filteredRandomArtworks = artworks.items.filter(
+    (item) => item.dimensions[0].value > item.dimensions[1].value
+  );
+
+  if (filteredRandomArtworks.length === 0) {
+    return null;
+  }
+
   const randomArtwork =
-    artworks.items[Math.floor(Math.random() * artworks.items.length)];
+    filteredRandomArtworks[
+      Math.floor(Math.random() * filteredRandomArtworks.length)
+    ];
   console.log(artworks);
   return (
     <>
       <Link href={`/artwork-info/${randomArtwork.id}`}>
-        <Image
+        <StyledImageList
           src={randomArtwork.image_thumbnail}
           alt={randomArtwork.titleText}
           width={200}
@@ -18,3 +29,8 @@ export default function RandomCover({ artworks }) {
     </>
   );
 }
+
+const StyledImageList = styled(Image)`
+  height: 100%;
+  width: 100%;
+`;
