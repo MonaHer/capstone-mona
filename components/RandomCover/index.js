a2bf0e52f055a162ac1cf9f803692ae3b4931846
Mona Herter <hertermona@gmail.com>
2023-08-01 20:54:32 +0200

    fix random cover bug, so only artworks with all keys and values are displayed

--- components/RandomCover/index.js
+++ components/RandomCover/index.js
@@ -7,10 +7,20 @@ import {
 } from "./styles";
 
 export default function RandomCover({ artworks }) {
-  const filteredRandomArtworks = artworks.items.filter(
-    (item) =>
-      item.dimensions && item.dimensions[0].value > item.dimensions[1].value
-  );
+  const filteredRandomArtworks = artworks.items.filter((item) => {
+    const hasDimensions = item.dimensions && item.dimensions.length >= 2;
+    const hasProductionDatesNotes =
+      item.production_dates_notes && item.production_dates_notes.length >= 1;
+    const hasCreator =
+      item.production &&
+      item.production[0] &&
+      item.production[0].creator_forename &&
+      item.production[0].creator_surname &&
+      item.production[0].creator_date_of_birth &&
+      item.production[0].creator_date_of_death;
+
+    return hasDimensions && hasProductionDatesNotes && hasCreator;
+  });
 
   if (filteredRandomArtworks.length === 0) {
     return null;
